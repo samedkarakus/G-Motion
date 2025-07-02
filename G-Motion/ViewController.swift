cb3332625580cefc87a008547473e8b1ebd55859
Samed Karakuş <91789542+samedkarakus@users.noreply.github.com>
2025-07-03 00:43:01 +0300

    Initial Commit

---
+++ G-Motion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G-Motion
+//
+//  Created by Samed Karakuş on 3.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
